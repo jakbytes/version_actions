ff81697d995cf560e47f030abff969c92b01a50c
Jake Kring <jacob.alan.kring@gmail.com>
2024-02-07 13:56:15 -0500

    debug(version): occasionally the list of commits does not stop at the correct hash, logging behavior

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,24 @@ module.exports = {
     rules: {
         'header-max-length': [2, 'always', 100],
         'body-max-line-length': [0, 'always', 100],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'build',
+                'chore',
+                'ci',
+                'docs',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test',
+                'debug'
+            ],
+        ],
     },
     extends: ['@commitlint/config-conventional']
 };
